fde8ee580b3e9d0b598bb1e4650154325f60c7a8
ryancope <ryan.cope34@gmail.com>
2021-01-03 16:04:36 -0600

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'The Dating App';
-  users: any;
+export class AppComponent implements OnInit {
+    title = 'The Dating App';
+    users: any;
 
-  constructor(private http: HttpClient) {}
-    
-  ngOnInit() {
-        this.getUsers();
+    constructor(private accountService: AccountService) { }
+
+    // tslint:disable-next-line: typedef
+    ngOnInit() {
+        this.setCurrentUser();
     }
 
-    getUsers() {
-        this.http.get('https://localhost:5001/api/users').subscribe(response => {
-            this.users=response;
-        }, error => {
-            console.log(error);
-        })
+    // tslint:disable-next-line: typedef
+    setCurrentUser() {
+        const user: User = JSON.parse(localStorage.getItem('user'));
+        this.accountService.setCurrentUser(user);
     }
+
 }
